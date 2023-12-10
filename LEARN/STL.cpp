6b4b39437c70434115f024d3b81f0ca88db07d56
Sudharshan Balaji <bsudharshan2001@gmail.com>
2023-12-09 19:38:56 -0500

    Learned queues in STL

--- LEARN/STL.cpp
+++ LEARN/STL.cpp
@@ -220,3 +220,24 @@ void example_stack(){
     st1.swap(st2);
 
 }
+
+
+void example_queue(){
+    //FIFO
+    queue<int>q;
+    q.push(1); // {1}
+    q.push(2); // {1, 2}
+    q.emplace(4);// {1, 2, 4}
+
+    q.back()+=5; // {1, 2, 9}
+
+    cout<<q.back(); //9
+
+    cout<<q.front(); //1
+
+    q.pop(); // {2,9}
+
+    cout<<q.front(); //2
+
+    //size swap empty -> same as stack
+}
