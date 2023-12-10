60185a6b729d71ffa6dce50cb422e60f6455910a
Sudharshan Balaji <bsudharshan2001@gmail.com>
2023-12-10 01:51:48 -0500

    Learned and added priority queues to STL

--- LEARN/STL.cpp
+++ LEARN/STL.cpp
@@ -241,3 +241,31 @@ void example_queue(){
 
     //size swap empty -> same as stack
 }
+
+
+void example_priorityQueue(){
+    priority_queue<int> pq;
+
+    pq.push(5); // {5}
+    pq.push(2); // {5,2}
+    pq.push(8); // {8,5,2}
+    pq.emplace(10); // {10,8,5,2}
+
+    cout<<pq.top(); // 10
+    
+    pq.pop(); // {8,5,2}
+
+    cout<<pq.top(); //8
+
+    //size swap empty function same as others
+
+    //Minimum heap
+    priority_queue<int, vector<int>, greater<int>> pq1;
+    pq1.push(5); // {5}
+    pq1.push(2); // {2,5}
+    pq1.push(8); // {2,5,8}
+    pq1.emplace(10); // {2,5,8,10}
+
+    cout<<pq1.top(); // 2
+
+}
