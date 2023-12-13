b436030cde98c4973e11561d3fc06667b0ad5517
Sudharshan Balaji <bsudharshan2001@gmail.com>
2023-12-12 23:53:01 -0500

    Added maps

--- LEARN/STL.cpp
+++ LEARN/STL.cpp
@@ -354,6 +354,26 @@ void example_uset(){
 
 void example_map(){
     map<int, int> mpp;
-    map<int, pair
+    map<int, pair<int, int>> mpp;
+    map<pair<int,int>,int> mpp;
+
+    mpp[1]=2;
+    // mpp.emplace({3,1});
+    // mpp.insert({2,4});
+
+    // mpp[{2,3}]=10;
+
+    // {
+    //     {1,2}
+    //     {2,4}
+    //     {3,1}
+    // }
+
+    for(auto it:mpp){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+
+    cout<<mpp[1];
+    cout<<mpp[5];
 }
 
