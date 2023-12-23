a83240255c63ca719ad825a8a018f5c6b31d51ab
Sudharshan Balaji <bsudharshan2001@gmail.com>
2023-12-22 23:43:05 -0500

    Update

---
+++ Strivers Sheet/Sorting/selection_sort.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[6]={13,46,24,52,20,9};
+    return 0;
+}
--- Strivers Sheet/gcd.cpp
+++ Strivers Sheet/gcd.cpp
@@ -1,16 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+// But this is the brute force approach - time complexity - O(n)
+// int main(){
+//     int a,b,n=1,flag;
+//     cin>>a>>b;
+//     // cout<<a<<b;
+//     while(n<a && n<b){
+//         if(a%n==0 && b%n==0)
+//             flag=n;
+//         // cout<<n;
+//         n++;
+//     }
+    // Alternate for loop
+    // for (int i = 1; i <= min(a, b); i++) {
+	// 	if (a % i == 0 && b % i == 0) {
+	// 		ans = i;
+	// 	}
+//     cout<<flag;
+//     // cout<<"hello";
+//     return 0;
+// }
+
+// Optimal Approach - Use Euclidean theorem
+// gcd(a,b)=gcd(b,a%b)
+// Time Complexity - O(log min(a,b))
+
+int gcd(int a, int b){
+    if(b==0)
+        return a;
+    else return gcd(b,a%b);
+}
+
 int main(){
-    int a,b,n=0,flag;
+    int a,b;
     cin>>a>>b;
-    while(n<a && n<b){
-        if(a%n==0 && b%n==0)
-            flag=n;
-        cout<<n;
-        n++;
-    }
-    cout<<flag;
-    cout<<"hello";
+    cout<<gcd(b,a%b);
     return 0;
 }
