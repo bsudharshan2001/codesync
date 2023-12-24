cf24824c45af021f77f3d27b56782ee2dce50026
Sudharshan Balaji <bsudharshan2001@gmail.com>
2023-12-24 02:01:25 -0500

    Sorting techniques

--- Strivers Sheet/Sorting/bubble_sort.cpp
+++ Strivers Sheet/Sorting/bubble_sort.cpp
@@ -1,7 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Time Complexity 
+// Worst case - O(n^2) - If we want to sort in ASC and the array is in DESC
+// Best case - O(n) - If its sorted
+// Avg case - O(n^2)
+
+// Use bubble sort
+// - If Complexity doesn't  matter
+// - For Simple code
+
 int main(){
-    int a[6]={13,46,24,52,20,9};
+    int n,temp;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    //Here in this algorithm, all the comparisons are make even if the array is already sorted -> Increases execution time
+    // We can introduce a extra var -> swapped
+    // for(int i=0;i<n-1;i++){
+    //     for(int j=0;j<n-i-1;j++){
+    //     if(a[j]>a[j+1]){
+    //         temp=a[j];
+    //         a[j]=a[j+1];
+    //         a[j+1]=temp;}
+    //     }
+    // }
+
+    //Optimized bubble sort
+     for(int i=0;i<n-1;i++){
+        int swapped=0;
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            
+                swapped=1;
+            }
+        }
+        if(swapped=0)
+            break;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
     return 0;
 }
--- Strivers Sheet/Sorting/selection_sort.cpp
+++ Strivers Sheet/Sorting/selection_sort.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main(){
-    int a[6]={13,46,24,52,20,9};
+    int n,temp,min;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    for(int i=0;i<n-1;i++){
+        min=i;
+        for(int j=i+1;j<n;j++){
+            if(a[j]<a[min])
+                min=j;
+        }
+        temp=a[i];
+        a[i]=a[min];
+        a[min]=temp;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
     return 0;
 }
