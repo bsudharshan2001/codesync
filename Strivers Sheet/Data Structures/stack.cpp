2ef248e6101ce1681846f3fa76eeaa2b507d91cb
Sudharshan Balaji <bsudharshan2001@gmail.com>
2024-01-03 00:59:17 -0500

    Learned Trees

---
+++ Strivers Sheet/Data Structures/stack.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAX 10
+
+int size=0;
+
+//Creating a Stack
+
+struct stack{
+    int items[MAX];
+    int top;
+};
+
+
+
+int main(){
+    int ch;
+
+    st *s=(st *) malloc(sizeof(st));
+
+    createEmptyStack(s);
+    
+    return 0;
+}
