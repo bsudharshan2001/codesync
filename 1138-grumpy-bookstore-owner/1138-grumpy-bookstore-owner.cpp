f3151f3053225616779324298c21efc348c78eb6
Sudharshan Balaji <bsudharshan2001@gmail.com>
2024-06-21 22:43:21 +0530

    Time: 21 ms (76.97%), Space: 34.3 MB (82.09%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n = customers.size();
-        
-        int total_satisfied = 0;
-        for (int i = 0; i < n; ++i) {
-            if (grumpy[i] == 0) {
-                total_satisfied += customers[i];
+        int l=0,window=0,satisfied=0, max_window=0;
+        for(int r=0;r<customers.size();r++){
+            if(grumpy[r]==1){
+                window+=customers[r];
             }
-        }
+            else satisfied+=customers[r];
 
-        int max_additional_satisfied = 0;
-        for (int i = 0; i <= n - minutes; ++i) {
-            int additional_satisfied = 0;
-            for (int j = 0; j < minutes; ++j) {
-                if (grumpy[i + j] == 1) {
-                    additional_satisfied += customers[i + j];
-                }
+            if(r-l+1>minutes){
+                if(grumpy[l])
+                    window-=customers[l];
+                l+=1;
             }
-            max_additional_satisfied = max(max_additional_satisfied, additional_satisfied);
-        }
+            max_window=max(window,max_window);
 
-        return total_satisfied + max_additional_satisfied;
+        }
+        return satisfied+max_window;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+//         int n = customers.size();
+        
+//         int total_satisfied = 0;
+//         for (int i = 0; i < n; ++i) {
+//             if (grumpy[i] == 0) {
+//                 total_satisfied += customers[i];
+//             }
+//         }
+
+//         int max_additional_satisfied = 0;
+//         for (int i = 0; i <= n - minutes; ++i) {
+//             int additional_satisfied = 0;
+//             for (int j = 0; j < minutes; ++j) {
+//                 if (grumpy[i + j] == 1) {
+//                     additional_satisfied += customers[i + j];
+//                 }
+//             }
+//             max_additional_satisfied = max(max_additional_satisfied, additional_satisfied);
+//         }
+
+//         return total_satisfied + max_additional_satisfied;
+//     }
+// };
+
