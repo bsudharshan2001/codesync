1b3004019b4a5d695eb393053742d9b71d7daa10
Sudharshan Balaji <bsudharshan2001@gmail.com>
2024-07-07 22:35:33 +0530

    Time: 3 ms (13.78%), Space: 7.1 MB (72.3%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int totalDrank = 0, emptyBottles = 0;
+        
+        while (numBottles > 0) {
+            totalDrank += numBottles;
+            emptyBottles += numBottles;
+            numBottles = emptyBottles / numExchange;
+            emptyBottles = emptyBottles % numExchange;
+        }
+        
+        return totalDrank;
+    }
+};
