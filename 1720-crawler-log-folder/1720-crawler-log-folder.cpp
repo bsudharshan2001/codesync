8555b517d417adb0b319b38f9101602601a2ffea
Sudharshan Balaji <bsudharshan2001@gmail.com>
2024-07-10 19:44:23 +0530

    Time: 4 ms (67.13%), Space: 14 MB (61.11%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -2,16 +2,35 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         int res=0;
-        regex pattern(R"((\w*\d+/)|(\d+/))");
         for(string &log:logs){
-            if(log=="../" && res>0)
-                res--;
+            if(log=="../")
+                {if(res>0)
+                res--;}
             else if(log=="./")
                 continue;
-            else if(regex_search(log, pattern))
-                res++;
+            else res++;
         }
+        if(res<0) return 0;
         return res;
         
     }
 };
+
+
+// class Solution {
+// public:
+//     int minOperations(vector<string>& logs) {
+//         int res=0;
+//         regex pattern(R"((\w*\d+/)|(\d+/))");
+//         for(string &log:logs){
+//             if(log=="../" && res>0)
+//                 res--;
+//             else if(log=="./")
+//                 continue;
+//             else if(regex_search(log, pattern))
+//                 res++;
+//         }
+//         return res;
+        
+//     }
+// };
