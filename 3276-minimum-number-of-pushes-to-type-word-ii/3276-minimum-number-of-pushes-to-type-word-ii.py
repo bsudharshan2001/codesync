69802f166ad940247546b7e69ecea457f1ec2084
Sudharshan Balaji <bsudharshan2001@gmail.com>
2024-08-07 01:42:21 +0530

    Time: 454 ms (5.04%), Space: 18.4 MB (8.15%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        lst=list(word)
+        lst.sort(reverse=True)
+        x=Counter(lst)
+        print(x)
+        cost=0
+        counter=1
+        multiplier=1
+
+        for i, j in x.most_common():
+            if(counter>8):
+                multiplier+=1
+                counter=1
+            cost+=multiplier*j
+            print(f"Count = {counter} ---- E = {i} * {j} - > Cost = {cost} --- Mul = {multiplier}\n")
+            counter+=1
+            
+
+        return cost
