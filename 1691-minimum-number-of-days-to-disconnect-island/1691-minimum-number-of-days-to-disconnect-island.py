bce8b110132397602c7918248044705dfaf50469
Sudharshan Balaji <bsudharshan2001@gmail.com>
2024-08-11 19:55:28 +0530

    Time: 1495 ms (12.15%), Space: 17.2 MB (29.91%) - LeetHub

---
+++ 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minDays(self, grid: List[List[int]]) -> int:
+
+        rows, cols = len(grid), len(grid[0])
+        def dfs(r,c,visit):
+            if(r<0 or c<0 or r==rows or c==cols or grid[r][c]==0 or (r,c) in visit):
+                return
+            visit.add((r,c))
+            neighbors=[[r+1,c],[r-1,c],[r,c+1],[r,c-1]]
+            for nr, nc in neighbors:
+                dfs(nr,nc, visit)
+
+        def helper():
+            visit=set()
+            count=0
+            for r in range(rows):
+                for c in range(cols):
+                    if(grid[r][c] and (r,c) not in visit):
+                        dfs(r,c, visit)
+                        count+=1
+            return count
+        
+        if(helper()!=1):
+            return 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if(grid[r][c]==0):
+                    continue
+                grid[r][c]=0
+                if(helper()!=1):
+                    return 1
+                grid[r][c]=1
+        
+        return 2
+
+        
