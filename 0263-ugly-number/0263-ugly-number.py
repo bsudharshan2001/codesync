0316942aa7ca5e71bb7258e8adc3e2c3bc3b99dd
Sudharshan Balaji <bsudharshan2001@gmail.com>
2024-08-23 23:34:38 +0530

    Time: 32 ms (80.2%), Space: 16.7 MB (7.67%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+
+        def helper(n):
+            if(n==1):
+                return True
+            if(n<=0):
+                return False
+
+            if(n%2==0):
+                return helper(n/2)
+            if(n%3==0):
+                return helper(n/3)
+            if(n%5==0):
+                return helper(n/5)
+            
+            return False
+
+        return helper(n)
+        
